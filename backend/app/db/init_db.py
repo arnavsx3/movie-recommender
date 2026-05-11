c6bd87b0d2daa41eae40d08735d802d01a0a4efc
arnavsx3 <arnavsx3@gmail.com>
2026-05-11 19:23:05 +0530

    fix lint errors

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,9 +1,10 @@
 from .database import engine, Base
-from . import models
+from . import models  # noqa:F401
 
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
 
 if __name__ == "__main__":
     init_db()
--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-from jose import JWTError, jwt
+from jose import jwt
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
@@ -25,7 +25,11 @@ def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY not set in .env")
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_token(token: str) -> dict:
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY not set in .env")
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
