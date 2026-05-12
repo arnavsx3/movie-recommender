2b57f301690bc98c0bf2d8329716688706bf09a4
arnavsx3 <arnavsx3@gmail.com>
2026-05-12 11:47:17 +0530

    fix passlib-bcrypt version compatibility

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from .api.routes.auth import router as auth_router
 
 app = FastAPI(
     title="Movie Recommender API",
     version="0.1.0"
 )
+
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
