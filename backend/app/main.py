f376750d2a178b252055dfb57463c2d24ffc4ddd
arnavsx3 <arnavsx3@gmail.com>
2026-05-18 23:49:57 +0530

    add ratings_router to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .api.routes.auth import router as auth_router
 from .api.routes.movies import router as movies_router
+from backend.app.api.routes.ratings import router as ratings_router
 
 app = FastAPI(
     title="Movie Recommender API",
@@ -9,6 +10,7 @@ app = FastAPI(
 
 app.include_router(auth_router)
 app.include_router(movies_router)
+app.include_router(ratings_router)
 
 @app.get("/")
 def root():
